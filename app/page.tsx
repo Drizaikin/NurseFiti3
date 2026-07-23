0d0c2939f08f8b4af788340f4160607669ca09c3
Drizaikin <danotyanga@gmail.com>
2026-07-23 19:26:22 +0300

    blog content updated

--- app/page.tsx
+++ app/page.tsx
@@ -380,7 +380,7 @@ export default async function Home() {
       .eq('is_approved', true)
       .order('is_pinned', { ascending: false })
       .order('created_at', { ascending: false })
-      .limit(8)
+      .limit(100)
   ]);
 
   const stats = [
